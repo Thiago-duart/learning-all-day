40a8924fc0ac0cce7e8c35229d51cb2e26f7f198
Thiago-duart <td1405150+github@gmail.com>
2024-10-11 09:03:05 -0300

    test: should test if an error is being returned when data is not provided

--- test/controllers/auth/auth.controller.spec.ts
+++ test/controllers/auth/auth.controller.spec.ts
@@ -1,14 +1,24 @@
 import { AuthController } from "../../../src/controllers/auth/auth.controller"
-import { IUserSingUp } from "../../../src/domain/models/user.models"
 
 describe('src/controllers/auth/auth.controllers.ts',()=>{
+
     const makeSut =()=>{
         const AuthStub = new AuthController()
         return {AuthStub}
     }
-test('testReceiveNameString_invalidEmail_returns400',async()=>{
+
+test('testReceiveName_nameNotFound_returns400',async()=>{
     const singUpData = {body:{
         email:'invalid-email',
+        password:'valid-password'
+    }}
+    const {AuthStub} = makeSut()
+    const response = await AuthStub.singUp(singUpData)
+    expect(response).toEqual({statusCode:400,body:'The name field is mandatory and was not provided.'})
+})
+test('testReceiveEmail_emailNotFound_returns400',async()=>{
+    const singUpData = {body:{
+        name:'valid-name',
         password:'valid-password'
     }}
     const {AuthStub} = makeSut()
