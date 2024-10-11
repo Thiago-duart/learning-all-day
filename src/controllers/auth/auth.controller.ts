7720ca6ea5f8d8de21b444668349b176dd6968d6
Thiago-duart <td1405150+github@gmail.com>
2024-10-11 09:01:56 -0300

    refactor: should improve data verification

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -3,6 +3,12 @@ import { IHttpRequest, IHttpResponse } from "../interfaces/http.interface";
 
 export class AuthController implements IAuth{
     async singUp(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-        return {body:'The email field is mandatory and was not provided.',statusCode:400}
+        const varifyData = ['name','email','password']
+        for (let i = 0; i < varifyData.length; i++) {
+            if(!httpRequest.body.hasOwnProperty(varifyData[i])){
+                return {body:`The ${varifyData[i]} field is mandatory and was not provided.`,statusCode:400}
+            }
+        }
+        return {body:'valid-return',statusCode:200}
     }
 }
